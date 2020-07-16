6300138ecb81bacee50c7d0ac351784d49a5d6ad
Harm De Weirdt <harm.deweirdt@mailfence.com>
2020-07-16 15:47:54 +0200

    add swagger metadata

--- src/main/java/be/harm/carshare/cars/config/SpringFoxConfig.java
+++ src/main/java/be/harm/carshare/cars/config/SpringFoxConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +22,21 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/cars/**"))
-                .build();
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Harm De Weirdt", "https://github.com/hdeweirdt", "harm.deweirdt@mailfence.com");
+
+        return new ApiInfo("Carshare cars API" ,
+                "Service regarding cars in the carshare platform",
+                "v0.0.1",
+                null,
+                contact,
+                null,
+                null,
+                Collections.emptyList());
     }
 }
 
