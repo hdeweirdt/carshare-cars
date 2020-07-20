0d9019a746bd66973f1c40258f5dd2c68a0dd8c2
Harm De Weirdt <harm.deweirdt@mailfence.com>
2020-07-20 17:01:59 +0200

    Enable service discovery with eureka

---
+++ src/main/java/be/harm/carshare/cars/config/LocalDiscovery.java
@@ -0,0 +1,10 @@
+package be.harm.carshare.cars.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableDiscoveryClient
+public class LocalDiscovery {
+}
